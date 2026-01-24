5ac1c8f5cc6965cf1253d3d70f5c852f24d01b53
Latynintest <79155090055@ya.ru>
2026-01-24 22:23:47 +0300

    Код изменен, добавлены новые файлы

--- src/test/java/com/skyrexio/tests/BaseTest.java
+++ src/test/java/com/skyrexio/tests/BaseTest.java
@@ -1,5 +1,7 @@
-package com.skyrexio;
+package com.skyrexio.tests;
 
+import java.time.Duration;
+import com.skyrexio.pages.*;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -7,18 +9,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-
 public class BaseTest {
     public WebDriver driver;
+    public LoginPage loginPage;
+    public ProductsPage productsPage;
 
     @BeforeMethod
     public void setup() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
-        /* options.addArguments("headless"); */
+        options.addArguments("--guest");
+        /*
+         * options.addArguments("--window-size=1920,1080"); options.addArguments("headless");
+         */
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
         driver.get("https://www.saucedemo.com/");
+        loginPage = new LoginPage(driver);
+        productsPage = new ProductsPage(driver);
     }
 
     @AfterMethod
